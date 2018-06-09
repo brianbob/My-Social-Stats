3abec6e6fb8a5b93e4fcfe188ce6cde26be5ae53
Brian Bridge <brianjbridge@gmail.com>
2018-06-09 13:35:56 -0400

    Working towards facebook integration

--- src/Controller/MySocialStatsController.php
+++ src/Controller/MySocialStatsController.php
@@ -12,9 +12,27 @@ class MySocialStatsController extends ControllerBase {
    * @return array
    */
   public function content() {
+    // Get the config object.
+    $config = \Drupal::config('my_social_stats.settings'); // at the top of the method
+    // Get our config values.
+    $id = $config->get('my_social_stats.app_id');
+    $secret = $config->get('my_social_stats.app_secret');
+
+    $fb = new \Facebook\Facebook([
+      'app_id' => $id,
+      'app_secret' => $secret,
+      'default_graph_version' => 'v2.10',
+      //'default_access_token' => '{access-token}', // optional
+    ]);
+
+    $helper = $fb->getRedirectLoginHelper();
+
+    $permissions = ['email']; // Optional permissions
+    $loginUrl = $helper->getLoginUrl('https://brianjbridge.com/fb-callback', $permissions);
+
     return [
       '#type' => 'markup',
-      '#markup' => $this->t('Hello, World!'),
+      '#markup' => $this->t("<a href='$loginUrl'>Log in with Facebook!</a>"),
     ];
   }
 
