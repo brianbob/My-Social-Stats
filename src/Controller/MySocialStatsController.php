a30f81a7dd9582d933d4242477737a5464b60227
Brian Bridge <brianjbridge@gmail.com>
2018-06-14 13:00:06 -0400

    got a connection to fb, working on some errors now.

--- src/Controller/MySocialStatsController.php
+++ src/Controller/MySocialStatsController.php
@@ -11,9 +11,9 @@ class MySocialStatsController extends ControllerBase {
    *
    * @return array
    */
-  public function content() {
+  public function stats_page() {
     // Get the config object.
-    $config = \Drupal::config('my_social_stats.settings'); // at the top of the method
+    $config = \Drupal::config('my_social_stats.settings');
     // Get our config values.
     $id = $config->get('my_social_stats.app_id');
     $secret = $config->get('my_social_stats.app_secret');
@@ -26,6 +26,7 @@ class MySocialStatsController extends ControllerBase {
     ]);
 
     $helper = $fb->getRedirectLoginHelper();
+    //_SESSION['FBRLH_state']=$_GET['state'];
 
     $permissions = ['email']; // Optional permissions
     $loginUrl = $helper->getLoginUrl('https://brianjbridge.com/fb-callback', $permissions);
@@ -36,4 +37,56 @@ class MySocialStatsController extends ControllerBase {
     ];
   }
 
+  public function fb_callback() {
+    // Get the config object.
+    $config = \Drupal::config('my_social_stats.settings');
+    // Get our config values.
+    $id = $config->get('my_social_stats.app_id');
+    $secret = $config->get('my_social_stats.app_secret');
+
+    $fb = new \Facebook\Facebook([
+      'app_id' => $id,
+      'app_secret' => $secret,
+      'default_graph_version' => 'v2.10',
+      //'default_access_token' => '{access-token}', // optional
+    ]);
+    // Login helper.
+    $helper = $fb->getRedirectLoginHelper();
+    //_SESSION['FBRLH_state']=$_GET['state'];
+    $message = '';
+
+    try {
+      $accessToken = $helper->getAccessToken();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+      // When Graph returns an error
+      $message .= 'Graph returned an error: ' . $e->getMessage();
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+      // When validation fails or other local issues
+      $message .=  'Facebook SDK returned an error: ' . $e->getMessage();
+    }
+
+    if (! isset($accessToken)) {
+      if ($helper->getError()) {
+        header('HTTP/1.0 401 Unauthorized');
+        $message .=  "Error: " . $helper->getError() . "\n";
+        $message .=  "Error Code: " . $helper->getErrorCode() . "\n";
+        $message .=  "Error Reason: " . $helper->getErrorReason() . "\n";
+        $message .=  "Error Description: " . $helper->getErrorDescription() . "\n";
+      } else {
+        //header('HTTP/1.0 400 Bad Request');
+        $message .=  'Bad request';
+      }
+    }
+    else {
+      $message .= $accessToken->getValue();
+    }
+
+    // The OAuth 2.0 client handler helps us manage access tokens
+    //$oAuth2Client = $fb->getOAuth2Client();
+
+    return [
+      '#type' => 'markup',
+      '#markup' => $message,
+    ];
+  }
 }
