aab1cb69dbc306a843abee7588e5c2001d8987a0
Brian Bridge <brianjbridge@gmail.com>
2018-08-27 16:51:56 -0400

    Revamping the settings page a little bit to use fieldsets.

--- src/Form/SocialSettings.php
+++ src/Form/SocialSettings.php
@@ -21,51 +21,9 @@ class SocialSettings extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     // Form constructor.
     $form = parent::buildForm($form, $form_state);
-    $facebook = new FacebookStats();
-     $message = "Please set your app ID and secret to generate a Facebook login link";
-    // Default settings.
-    $config = $this->config('my_social_stats.settings');
-    $app_id = $config->get('my_social_stats.app_id');
-    $app_secret = $config->get('my_social_stats.app_secret');
-    // App ID field.
-    $form['app_id'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('App ID:'),
-      '#default_value' => $app_id,
-    );
-    // App secret field
-    $form['app_secret'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('App Secret:'),
-      '#default_value' => $app_secret,
-    );
-    // Start Date field
-    $form['start_date'] = array(
-      '#type' => 'date',
-      '#title' => $this->t('Start Date:'),
-      '#default_value' => $config->get('my_social_stats.start_date'),
-      '#date_format' => 'Y-m-d',
-      '#date_increment' => 1,
-      '#date_year_range' => '-3:+3',
-    );
-    // Check to make sure the app credentials are set before using our FB class.
-    if(isset($app_id) && isset($app_secret)) {
-      if($facebook->amILoggedIn()) {
-        // If we're logged in, get update our data.
-        // @TODO make this configurable. I'm thinking a 'refresh' button?
-        $facebook->getData();
-        $message = "You are logged in. Your data has been refreshed.";
-      }
-      else {
-        // If we're not logged in get a login link.
-        $message =  $facebook->getLoginLink();
-      }
-    }
 
-    $form['facebook_login'] = array(
-      '#type' => 'markup',
-      '#markup' => $message,
-    );
+    $this->createFacebookFieldset($form);
+
     return $form;
   }
 
@@ -81,9 +39,9 @@ class SocialSettings extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('my_social_stats.settings');
-    $config->set('my_social_stats.app_id', $form_state->getValue('app_id'));
-    $config->set('my_social_stats.app_secret', $form_state->getValue('app_secret'));
-    $config->set('my_social_stats.start_date', $form_state->getValue('start_date'));
+    $config->set('my_social_stats.facebook_app_id', $form_state->getValue('app_id'));
+    $config->set('my_social_stats.facebook_app_secret', $form_state->getValue('app_secret'));
+    $config->set('my_social_stats.facebook_start_date', $form_state->getValue('start_date'));
     $config->save();
     return parent::submitForm($form, $form_state);
   }
@@ -97,4 +55,69 @@ class SocialSettings extends ConfigFormBase {
     ];
   }
 
+  /**
+   * Creates the Facebook-specific fieldset and fields needed to connect to the API.
+   *
+   * @param  array &$form
+   *   The drupal form array to add the fields into.
+   */
+  private function createFacebookFieldset(&$form) {
+    $facebook = new FacebookStats();
+    $message = "Please set your app ID and secret to generate a Facebook login link";
+
+    // Default settings.
+    $config = $this->config('my_social_stats.settings');
+    $app_id = $config->get('my_social_stats.facebook_app_id');
+    $app_secret = $config->get('my_social_stats.facebook_app_secret');
+
+    // Create the facebook fieldset.
+    $form['facebook'] = array(
+      '#type' => 'details',
+      '#title' => t('Facebook'),
+      '#open' => FALSE,
+    );
+
+    // App ID field.
+    $form['facebook']['app_id'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('App ID:'),
+      '#default_value' => $app_id,
+    );
+
+    // App secret field
+    $form['facebook']['app_secret'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('App Secret:'),
+      '#default_value' => $app_secret,
+    );
+
+    // Start Date field
+    $form['facebook']['start_date'] = array(
+      '#type' => 'date',
+      '#title' => $this->t('Start Date:'),
+      '#default_value' => $config->get('my_social_stats.facebook_start_date'),
+      '#date_format' => 'Y-m-d',
+      '#date_increment' => 1,
+      '#date_year_range' => '-3:+3',
+    );
+
+    // Check to make sure the app credentials are set before using our FB class.
+    if(isset($app_id) && isset($app_secret)) {
+      if($facebook->amILoggedIn()) {
+        // If we're logged in, get update our data.
+        // @TODO make this configurable. I'm thinking a 'refresh' button?
+        $facebook->getData();
+        $message = "You are logged in. Your data has been refreshed.";
+      }
+      else {
+        // If we're not logged in get a login link.
+        $message =  $facebook->getLoginLink();
+      }
+    }
+
+    $form['facebook']['facebook_login'] = array(
+      '#type' => 'markup',
+      '#markup' => $message,
+    );
+  }
 }
